9de6224520dbd37bd142dcf2491af1d4f23991af
Sasha <alexpozitiv006@gmail.com>
2024-10-30 18:51:29 +0200

    updated API tests

--- src/main/java/org/example/PetBody.java
+++ src/main/java/org/example/PetBody.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class PetBody {
+    private int id;
+    private String name;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,4 +1,86 @@
-package PACKAGE_NAME;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class APITests {
+
+    @BeforeMethod
+    public void setup() {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+    }
+
+    @Test
+    public void testCreatingPet() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 286,\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"string\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"Yason\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"sold\"\n" +
+                        "}")
+                .log().all()
+                .when()
+                .post("/pet")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+    @Test
+    public void testUpdatingPet() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 286,\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"string\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"Yason(Updated)\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"sold\"\n" +
+                        "}")
+                .log().all()
+                .when()
+                .put("/pet")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+    @Test
+    public void testDeletingPet() {
+        given()
+                .log().all()
+                .when()
+                .delete("/pet/286")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
 }
